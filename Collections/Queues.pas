164d32e72cb70e870b8eae76e974af58de9d163d
StefanH <personalnexus@users.noreply.github.com>
2020-07-03 19:33:12 +0200

    * Added TConcurrentQueue * Built a poor man's test project without DUnit for concurrency tests

---
+++ Collections/Queues.pas
@@ -0,0 +1,111 @@
+unit Queues;
+    
+interface
+
+type
+  TConcurrentQueueItem<TValue> = class
+    FValue: TValue;
+    FNext:  TConcurrentQueueItem<TValue>;
+
+    constructor Create(const AValue: TValue);
+  end;
+
+  TConcurrentQueue<TValue> = class(TObject)
+  private
+    FHead:  TConcurrentQueueItem<TValue>;
+    FTail:  TConcurrentQueueItem<TValue>;
+    FCount: Integer;
+
+  public
+    constructor Create;
+
+    procedure Enqueue(const AValue: TValue);
+    function TryDequeue(out AValue: TValue): Boolean;
+
+    property Count: Integer read FCount;
+  end;
+
+
+implementation
+
+uses
+  Windows;
+
+// TConcurrentQueueItem<TValue>
+
+constructor TConcurrentQueueItem<TValue>.Create(const AValue: TValue);
+begin
+  inherited Create;
+  FValue := AValue;
+end;
+
+// TConcurrentQueue<TValue>
+
+constructor TConcurrentQueue<TValue>.Create;
+begin
+  inherited Create;
+  FTail := TConcurrentQueueItem<TValue>.Create(Default(TValue));
+  FHead := FTail;
+end;
+
+procedure TConcurrentQueue<TValue>.Enqueue(const AValue: TValue);
+var
+  CurrentTail:  TConcurrentQueueItem<TValue>;
+  NewTail:      TConcurrentQueueItem<TValue>;
+  PreviousTail: TConcurrentQueueItem<TValue>;
+begin
+  NewTail := TConcurrentQueueItem<TValue>.Create(AValue);
+  repeat
+    CurrentTail  := FTail;
+    PreviousTail := InterlockedCompareExchangePointer(Pointer(FTail), NewTail, CurrentTail);
+  until (PreviousTail = CurrentTail);
+  PreviousTail.FNext := NewTail;
+  //
+  // Incrementing FCount releases the new tail making it possible to dequeue
+  //
+  InterlockedIncrement(FCount);
+end;
+
+function TConcurrentQueue<TValue>.TryDequeue(out AValue: TValue): Boolean;
+var
+  CurrentCount:  Integer;
+  NewCount:      Integer;
+  PreviousCount: Integer;
+  CurrentHead:   TConcurrentQueueItem<TValue>;
+  NewHead:       TConcurrentQueueItem<TValue>;
+  PreviousHead:  TConcurrentQueueItem<TValue>;
+begin
+  repeat
+    CurrentCount := FCount;
+    if (CurrentCount = 0) then begin
+      PreviousCount := 0;
+    end else begin
+      Result        := True;
+      NewCount      := CurrentCount - 1;
+      PreviousCount := InterlockedCompareExchange(FCount, NewCount, CurrentCount);
+    end;
+  until (PreviousCount = CurrentCount);
+  //
+  // Only continue when we know there is an item to dequeue, because we were
+  // able to decrement FCount by 1 while CurrentCount remained greater than 0.
+  //
+  if (CurrentCount = 0) then begin
+    Result := False;
+  end else begin
+    Result := True;
+    repeat
+      CurrentHead  := FHead;
+      NewHead      := FHead.FNext;
+      PreviousHead := InterlockedCompareExchangePointer(Pointer(FHead), NewHead, CurrentHead);
+    until (PreviousHead = CurrentHead);
+    //
+    // There can be only thread that successfully swaps in NewHead, so this
+    // thread is entitled to use NewHead's FValue and free PreviousHead.
+    //
+    AValue := NewHead.FValue;
+    PreviousHead.Free;
+  end;
+end;
+
+
+end.
